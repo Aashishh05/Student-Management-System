42ad8a45d5951ec8aeddbae9bb0e8a59777b6522
Aashishh05 <shresthaaashish246@gmail.com>
2026-05-01 21:13:00 +0545

    Dashboard modified

--- Frontend/src/pages/Dashboard.jsx
+++ Frontend/src/pages/Dashboard.jsx
@@ -18,17 +18,17 @@ const Dashboard = () => {
       <div className="flex-1 flex flex-col min-w-0">
         <Header toggleSidebar={() => setIsOpen(!isOpen)} />
 
-        <main className="p-4 md:p-10">
+        <main className="p-4 md:p-10 mt-15">
           <div className="px-2 md:px-10 lg:px-20">
             
-            <div className="w-full bg-slate-200 rounded-2xl shadow-2xl p-6 md:border-l-[10px] border-b-[6px] border-[#2249A3]">
+            <div className="w-full bg-slate-200/10 rounded-2xl shadow-2xl p-6 md:border-l-[10px] border-b-[6px] border-[#2249A3]">
               <h1 className="text-3xl md:text-5xl font-bold bg-gradient-to-r from-slate-800 to-blue-600 bg-clip-text text-transparent mb-8 md:mb-12 font-serif ">
                 Dashboard Overview
               </h1>
 
               <div className="grid grid-cols-1 md:grid-cols-2 gap-6 md:gap-10">
                 
-                <div className="bg-gradient-to-r from-slate-700 to-blue-600 rounded-2xl p-6 md:p-10 cursor-pointer hover:scale-105 transition-all duration-300 shadow-xl flex justify-between items-center text-white">
+                <div className="bg-gradient-to-r from-slate-700 to-blue-600 rounded-2xl p-6 md:p-10 cursor-pointer hover:scale-110 transition-all duration-300 shadow-xl flex justify-between items-center text-white">
                   <div>
                     <h2 className="text-lg md:text-xl font-semibold font-serif opacity-90 uppercase tracking-widest">
                       Students
@@ -40,7 +40,7 @@ const Dashboard = () => {
                   </div>
                 </div>
 
-                <div className="bg-gradient-to-r from-purple-600 to-indigo-500 rounded-2xl p-6 md:p-10 cursor-pointer hover:scale-105 transition-all duration-300 shadow-xl flex justify-between items-center text-white">
+                <div className="bg-gradient-to-r from-purple-600 to-indigo-500 rounded-2xl p-6 md:p-10 cursor-pointer hover:scale-110 transition-all duration-300 shadow-xl flex justify-between items-center text-white">
                   <div>
                     <h2 className="text-lg md:text-xl font-semibold font-serif opacity-90 uppercase tracking-widest">
                       Teachers
