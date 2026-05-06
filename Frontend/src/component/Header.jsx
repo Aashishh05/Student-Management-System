4653388f3bceb7b0b0152710a7a150fcac1ec310
Aashishh05 <shresthaaashish246@gmail.com>
2026-05-06 11:43:39 +0545

    added search box

--- Frontend/src/component/Header.jsx
+++ Frontend/src/component/Header.jsx
@@ -1,7 +1,7 @@
 import React, { useState } from "react";
 import { NavLink } from "react-router-dom";
 import { FaRegCircleUser } from "react-icons/fa6";
-import { IoMenuSharp } from "react-icons/io5";
+import { IoMenuSharp, IoSearch } from "react-icons/io5";
 
 const Header = ({ toggleSidebar }) => {
   const [dropdown, setDropdown] = useState(false);
@@ -10,11 +10,11 @@ const Header = ({ toggleSidebar }) => {
     <header className="sticky w-full px-10 py-8 bg-gradient-to-r from-slate-400/60 to-white/50 backdrop-blur-xl shadow-2xl shadow-indigo-900/50 border-r border-none transition-colors font-serif">
       <div className="max-w-7xl mx-auto flex items-center justify-between">
         <div className="flex items-center gap-4">
-          <button 
+          <button
             onClick={toggleSidebar}
             className="text-2xl text-slate-700 hover:text-indigo-600 transition-colors"
           >
-            <IoMenuSharp className="text-3xl"/>
+            <IoMenuSharp className="text-3xl" />
           </button>
           <div>
             <h1 className="font-semibold text-3xl bg-gradient-to-r from-slate-800 to-blue-600 bg-clip-text text-transparent ">
@@ -26,28 +26,15 @@ const Header = ({ toggleSidebar }) => {
           </div>
         </div>
 
-        <nav className="hidden lg:flex gap-8">
-          <NavLink
-            to="/Student"
-            className="relative px-2 py-1 text-lg font-bold bg-gradient-to-r from-slate-800 to-blue-600 bg-clip-text text-transparent  after:absolute after:bottom-0 after:left-0 after:h-[2px] after:w-0 after:bg-indigo-600 after:transition-all hover:after:w-full"
-          >
-            Students
-          </NavLink>
-
-          <NavLink
-            to="/Teacher"
-            className="relative px-2 py-1 text-lg font-bold bg-gradient-to-r from-slate-800 to-blue-600 bg-clip-text text-transparent  after:absolute after:bottom-0 after:left-0 after:h-[2px] after:w-0 after:bg-indigo-600 after:transition-all hover:after:w-full"
-          >
-            Teachers
-          </NavLink>
-
-          <NavLink
-            to="/Courses"
-            className="relative px-2 py-1 text-lg font-bold bg-gradient-to-r from-slate-800 to-blue-600 bg-clip-text text-transparent  after:absolute after:bottom-0 after:left-0 after:h-[2px] after:w-0 after:bg-indigo-600 after:transition-all hover:after:w-full"
-          >
-            Courses
-          </NavLink>
-        </nav>
+        <div className="flex items-center bg-white/50 backdrop-blur-md border border-none rounded-xl px-3 py-2 shadow-2xl focus-within:ring-1 hover:shadow-sky-300 focus-within:ring-indigo-400 transition-all">
+          <IoSearch className="text-xl text-slate-500 mr-2" />
+          <input
+            type="search"
+            
+            placeholder="Search here..."
+            className="bg-transparent outline-none text-slate-700 placeholder-slate-400 w-70"
+          />
+        </div>
 
         <div className="relative">
           <div
